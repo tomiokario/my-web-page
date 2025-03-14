5d184de9ce34a07e0d870463d5ef3bcb959d4bfd
tomiokario <riopf239@gmail.com>
2025-03-14 16:14:39 +0900

    add: google font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/variables.css";
+import "./styles/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
